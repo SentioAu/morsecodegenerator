6169839eb21bd66dfcb4262c8177d7cf45621e69
SentioAu <faisalalrowayshed@gmail.com>
2025-12-26 03:29:08 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://morsecodegenerator.com',
+  site: "https://morsecodegenerator.com",
   integrations: [sitemap()],
+  trailingSlash: "always",
 });
